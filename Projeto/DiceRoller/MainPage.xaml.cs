ea21b207f1083a15f81364526390c6b7099db790
Alexis <alexisebastianarapa@gmail.com>
2024-04-10 15:22:46 -0300

    Upload do projeto - Finalizado

---
+++ Projeto/DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            int lados = Convert.ToInt32(Picker_Lados.SelectedItem);
+
+            Random random = new Random();
+
+            int aleatorio = random.Next(1, lados + 1);
+
+            Numero.Text = aleatorio.ToString();
+        }
+    }
+
+}
