66f02274edfb7b0d19ea34ddf8f6d6f793f433c4
Alexis <alexissebastianarapa@gmail.com>
2024-05-08 14:32:53 -0300

    uppload

---
+++ Projeto/DiceRoller/Models/Dice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller.Models
+{
+    
+    public class Dice
+    {
+        //Concstrução da classe Dice
+    }
+}
